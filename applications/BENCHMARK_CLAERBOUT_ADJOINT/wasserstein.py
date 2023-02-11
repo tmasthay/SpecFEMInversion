75cb41dcb59c8c8ced2db3b53f9999ae849ebabe
tmasthay <tyler@ices.utexas.edu>
2023-02-10 19:42:16 -0600

    Add cumulative/quantile functions: UNTESTED

--- applications/BENCHMARK_CLAERBOUT_ADJOINT/wasserstein.py
+++ applications/BENCHMARK_CLAERBOUT_ADJOINT/wasserstein.py
@@ -4,3 +4,21 @@ from itertools import accumulate
 def cumulative(u):
     v = np.array(list(accumulate(u)))
     return v / v[-1]
+
+def quantile(U, dp, dt):
+    curr = 0
+    Q = np.zeros(int(np.ceil(1/dp + 1)))
+    p = 0.0
+    inv_dp = 1.0 / dp
+    q = 1
+    t = 0
+    while( p <= 1.0 ):
+        if( t >= len(U) - 1 ): Q[q] = (len(U) - 1) * dt
+        else:
+            while( U[t+1] < p and t < len(U) ): t += 1
+            if( t == len(U) - 1 ): Q[q] = (len(U) - 1)*dt
+            else:
+                Q[q] = dt * (t + (p-U[t]) / (U[t+1] - U[t]))
+        q += 1
+        p += dp
+    return Q
