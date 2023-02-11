0112cbdc44bcdcba9a45844034ec79adfc7b2104
tmasthay <tyler@ices.utexas.edu>
2023-02-11 00:02:16 -0600

    Add unit test dummies

---
+++ applications/BENCHMARK_CLAERBOUT_ADJOINT/unit.py
@@ -0,0 +1,16 @@
+from wasserstein import *
+
+def test_cdf():
+    assert 1 == 1, "Incorrect CDF evalulation"
+
+def test_quantile():
+    assert 1 == 1, "Incorrect quantile evalulation"
+
+def test_transport_distance():
+    assert 1 == 1, "Incorrect Wasserstein kernel"
+
+def test_wass_adjoint():
+    assert 1 == 1, "Incorrect Wasserstein adjoint"
+
+def test_wass_adjoint_and_eval():
+    assert 1 == 1, "Incorrect Wasserstein adjoint+eval"
