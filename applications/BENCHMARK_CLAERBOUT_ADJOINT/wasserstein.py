ada212f7f647ddf98b75150fdfdec9f085aab85b
tmasthay <tyler@ices.utexas.edu>
2023-02-10 23:11:19 -0600

    Pass generalized vector to quantile for efficiency

--- applications/BENCHMARK_CLAERBOUT_ADJOINT/wasserstein.py
+++ applications/BENCHMARK_CLAERBOUT_ADJOINT/wasserstein.py
@@ -5,20 +5,21 @@ def cumulative(u):
     v = np.array(list(accumulate(u)))
     return v / v[-1]
 
-def quantile(U, dp, dt):
-    curr = 0
-    Q = np.zeros(int(np.ceil(1/dp + 1)))
-    p = 0.0
-    inv_dp = 1.0 / dp
+def quantile(U, p, dt):
+    Q = np.zeros(int(np.ceil(1.0/dp + 1)))
     q = 1
     t = 0
-    while( p <= 1.0 ):
+    for pp in p:
         if( t >= len(U) - 1 ): Q[q] = (len(U) - 1) * dt
         else:
-            while( U[t+1] < p and t < len(U) ): t += 1
+            while( U[t+1] < pp and t < len(U) ): t += 1
             if( t == len(U) - 1 ): Q[q] = (len(U) - 1)*dt
             else:
                 Q[q] = dt * (t + (p-U[t]) / (U[t+1] - U[t]))
         q += 1
-        p += dp
     return Q
+
+def wass_adjoint(u,d,dt):
+    alpha = 0.9
+    du = []
+    D = quantile
