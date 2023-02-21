5aa5b118c6ec42ab797f65cde91c3b9efa4adc17
tmasthay <tyler@ices.utexas.edu>
2023-02-20 22:27:49 -0600

    Protect helper_tyler

--- applications/BENCHMARK_CLAERBOUT_ADJOINT/ELASTIC/clear_dir.sh
+++ applications/BENCHMARK_CLAERBOUT_ADJOINT/ELASTIC/clear_dir.sh
@@ -6,10 +6,12 @@ rm -rf DATA/*bin
 rm -rf DATA/*jpg
 rm -rf SEM
 mv inversion.py inversion.tmp
+mv helper_tyler.py helper_tyler.tmp
 rm -f *.py
 rm change*.pl
 rm -rf bin
 mv inversion.tmp inversion.py
+mv helper_tyler.tmp helper_tyler.py
 
 #restore previous state of DATA/Par_file
 cp DATA/Par_file_ref DATA/Par_file
